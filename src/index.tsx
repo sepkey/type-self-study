ce4dba3a78a073871cca0ab13fc3849ae9866347
Sepide Kia <sepidekia6990@gmail.com>
2023-05-03 21:24:07 +0330

    webpack manually configuration

--- src/index.tsx
+++ src/index.tsx
@@ -5,7 +5,7 @@ const root = createRoot(document.getElementById("root") as HTMLElement);
 ///This is called a type assertion, which tells TypeScript what the type should be. Without the type assertion, TypeScript will infer the type as HTMLElement | null because document. getElementById may not find an element and return null. However, we are confident that the element will be found because we specified it in the index.html file, so it is safe to narrow the type to HTMLElement using a type assertion
 
 function App() {
-  return <h1>My React and TypeScript App!</h1>;
+  return <h1>My Super React and TypeScript App!</h1>;
 }
 root.render(
   <StrictMode>
