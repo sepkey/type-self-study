b44cb3420b36cab13adeb0c7f88c58e85f211691
Sepide Kia <sepidekia6990@gmail.com>
2023-05-05 22:00:18 +0330

    CSS in JS Styles for a CSS-in-JS library are defined in JavaScript.
    Emotion’s styles can be defined on a JSX element using a css attribute.
    
    Conditional logic can be added directly to the styles.
    
    Emotion styles are applied at runtime rather than at build time because they depend on JavaScript variables. While this allows conditional styling logic to be elegantly defined, it does mean a small performance penalty because the styles are created and applied at runtime.

--- myapp/src/App.tsx
+++ myapp/src/App.tsx
@@ -3,7 +3,7 @@ import { Alert } from './Alert';
 
 function App() {
   return (
-    <div className="App">
+    <div className="container">
       <Alert heading="success" closable>
         I'm feeling good
       </Alert>
