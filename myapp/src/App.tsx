babe935ce92efcd0af9d5723513b5a890e996f3d
Sepide Kia <sepidekia6990@gmail.com>
2023-05-09 23:47:23 +0330

    useState hook-first example

--- myapp/src/App.tsx
+++ myapp/src/App.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import './App.css';
 import { PersonScore } from './PersonScore';
 function App() {
   return (
--- myapp/src/PersonScore.tsx
+++ myapp/src/PersonScore.tsx
@@ -1,19 +1,37 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { getPerson } from './getPerson';
 
 export function PersonScore() {
+  const [name, setName] = useState<string | undefined>();
+  const [score, setScore] = useState(0);
+  const [loading, setLoading] = useState(true);
   useEffect(() => {
-    getPerson().then((person) => console.log(person));
+    getPerson().then((person) => {
+      console.log(person);
+      setLoading(false);
+      setName(person.name);
+      // console.log('State values', loading, name);
+    });
   }, []);
 
-  return null;
+  //this is under useEffect because of hooks rules
+  if (loading) return <div>...loading</div>;
+
+  return (
+    <div>
+      <p>jhgbkgb</p>
+      <h3>
+        {name}, {score}
+      </h3>
+      {/* <button onClick={() => setScore((prev) => prev + 1)}>Add</button>
+      <button onClick={() => setScore((prev) => prev - 1)}>Subtract</button> */}
+      {/* more readable */}
+      <button onClick={() => setScore(score + 1)}>Add</button>
+      <button onClick={() => setScore(score - 1)}>Subtract</button>
+      <button onClick={() => setScore(0)}>Reset</button>
+    </div>
+  );
 }
 
-////////// less readable than the initial version
-// useEffect(() => {
-//   async function getThePerson() {
-//     const person = await getPerson();
-//     console.log(person);
-//   }
-//   getThePerson();
-// }, []);
+//if you put the return value of loading null, and then hover over the component you will see that the return type is null or JSX.Element
+//So notice that in this conditional cases it's better not to annotate return type explicitly
