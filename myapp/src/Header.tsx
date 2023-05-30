3abfe6ff15a597b0031a9c0826d8e77fa2d3a15b
Sepide Kia <sepidekia6990@gmail.com>
2023-05-30 06:10:35 +0330

    add search feature with useSearchParams and then with usenavigate

--- myapp/src/Header.tsx
+++ myapp/src/Header.tsx
@@ -1,12 +1,35 @@
-import { Link, NavLink } from 'react-router-dom';
+import { Link, NavLink, useSearchParams, useNavigate } from 'react-router-dom';
+import { FormEvent } from 'react';
 import logo from './image/logo.png';
 
 export function Header() {
+  // const [searchParams, setSearchParams] = useSearchParams();
+  const [searchParams] = useSearchParams();
+
+  const nav = useNavigate();
+  function handleSearchSubmit(e: FormEvent<HTMLFormElement>) {
+    e.preventDefault();
+    const formData = new FormData(e.currentTarget);
+    const search = formData.get('search') as string;
+
+    // setSearchParams({ search: search });
+    nav(`/products/?search=${search}`);
+  }
   return (
     <header
       className="text-center text-slate-900
   bg-slate-200 h-40 p-5"
     >
+      <form className="relative text-right" onSubmit={handleSearchSubmit}>
+        <input
+          type="search"
+          name="search"
+          placeholder="search"
+          defaultValue={searchParams.get('search') ?? ''}
+          className="absolute right-0 top-0 rounded py-2 px-3
+text-gray-700"
+        />
+      </form>
       <Link to="">
         <img src={logo} alt="Logo" className="inline-block h-20" />
       </Link>
--- myapp/src/pages/ProductsPage.tsx
+++ myapp/src/pages/ProductsPage.tsx
@@ -1,12 +1,24 @@
 import { products } from '../data/products';
-import { Link } from 'react-router-dom';
-
+import { Link, useSearchParams } from 'react-router-dom';
 export function ProductsPage() {
+  const [searchParams] = useSearchParams();
+
+  function getFilteredProducts() {
+    const search = searchParams.get('search');
+    if (!search) {
+      return products;
+    } else {
+      return products.filter(
+        (product) => product.name.toLowerCase().indexOf(search.toLowerCase()) > -1,
+      );
+    }
+  }
+
   return (
     <div className="text-center p-5">
       <h2 className="text-xl font-bold text-slate-600">Here are some great tools for React</h2>
       <ul className="list-none m-0 p-0">
-        {products.map((product) => (
+        {getFilteredProducts().map((product) => (
           <li key={product.id} className="p-1 text-base textslate-800">
             <Link
               to={`${product.id}`}
