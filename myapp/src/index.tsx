a9ba1b3de8cf9b9b62ab28181a19e84abf9b2d7c
Sepide Kia <sepidekia6990@gmail.com>
2023-06-01 22:20:34 +0330

    save

--- myapp/src/index.tsx
+++ myapp/src/index.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import { Routes } from './Routes';
+// import { Routes } from './Routes';//Router-chapter
+import App from './form/App'; //Form-chapter
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
   <React.StrictMode>
-    <Routes />
+    {/* <Routes /> */}
+    <App />
   </React.StrictMode>,
 );
