ab9a6249974cf4b9f262b3c48cb49298578ec8ee
Sepide Kia <sepidekia6990@gmail.com>
2023-06-02 06:39:08 +0330

    add 3 type form: controlled-uncontrolled and React router Form

--- myapp/src/form/App.tsx
+++ myapp/src/form/App.tsx
@@ -1,7 +1,25 @@
-import ContactPage from './ContactPage';
+import ContactPage, { contactPageAction } from './ContactPage';
+import { createBrowserRouter, RouterProvider, Navigate } from 'react-router-dom';
+import ThankYouPage from './ThankYouPage';
 
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Navigate to="contact" />,
+  },
+  {
+    path: '/contact',
+    element: <ContactPage />,
+    action: contactPageAction,
+  },
+  {
+    path: '/thank-you/:name',
+    element: <ThankYouPage />,
+  },
+]);
 const App = () => {
-  return <ContactPage />;
+  // return <ContactPage />; //controled and uncontrolled form
+  return <RouterProvider router={router} />;
 };
 
 export default App;
--- myapp/src/form/ContactPage.tsx
+++ myapp/src/form/ContactPage.tsx
@@ -92,7 +92,85 @@
 //////////uncontroled by state///////
 /////////////////////////////////////
 
-import { useState, FormEvent } from 'react';
+// import { FormEvent } from 'react';
+// type Contact = {
+//   name: string;
+//   email: string;
+//   reason: string;
+//   notes: string;
+// };
+// const fieldStyle = 'flex flex-col mb-2';
+// const ContactPage = () => {
+//   function submitHandler(e: FormEvent<HTMLFormElement>) {
+//     e.preventDefault();
+//     const formData = new FormData(e.currentTarget);
+//     const contact = {
+//       name: formData.get('name'),
+//       email: formData.get('email'),
+//       reason: formData.get('reason'),
+//       notes: formData.get('notes'),
+//     } as Contact;
+//     console.log('Submitted details:', contact);
+//   }
+//   return (
+//     <div
+//       className="flex flex-col py-10 max-w-md
+//   mx-auto"
+//     >
+//       <h2
+//         className="text-3xl font-bold underline
+//   mb-3"
+//       >
+//         Contact Us
+//       </h2>
+//       <p className="mb-3">If you enter your details we'll get back to you as soon as we can.</p>
+//       <form onSubmit={submitHandler}>
+//         <div className={fieldStyle}>
+//           <label htmlFor="name">Your name</label>
+//           <input type="text" id="name" name="name" />
+//         </div>
+//         <div className={fieldStyle}>
+//           <label htmlFor="email">Your email address</label>
+//           <input type="text" id="email" name="email" />
+//         </div>
+
+//         <div className={fieldStyle}>
+//           <label htmlFor="reason">Reason you need to contact us</label>
+//           <select id="reason" name="reason">
+//             <option value=""></option>
+//             <option value="Support">Support</option>
+//             <option value="Feedback">Feedback</option>
+//             <option value="Other">Other</option>
+//           </select>
+//         </div>
+
+//         <div className={fieldStyle}>
+//           <label htmlFor="notes">Additional notes</label>
+//           <textarea id="notes" name="notes" />
+//         </div>
+
+//         <div className={fieldStyle}>
+//           <button
+//             type="submit"
+//             className="mt-2 h-10 px-6 font-semibold bg-black
+// text-white"
+//           >
+//             Submit
+//           </button>
+//         </div>
+//       </form>
+//     </div>
+//   );
+// };
+
+// export default ContactPage;
+
+/////////////////////////////////////
+//////////React router Form///////
+/////////////////////////////////////
+
+import { Form, ActionFunctionArgs, redirect } from 'react-router-dom';
+
 type Contact = {
   name: string;
   email: string;
@@ -101,12 +179,6 @@ type Contact = {
 };
 const fieldStyle = 'flex flex-col mb-2';
 const ContactPage = () => {
-  const [contact, setContact] = useState<Contact>({ name: '', email: '', notes: '', reason: '' });
-
-  function submitHandler(e: FormEvent<HTMLFormElement>) {
-    e.preventDefault();
-    console.log('Submitted details:', contact);
-  }
   return (
     <div
       className="flex flex-col py-10 max-w-md
@@ -119,33 +191,19 @@ const ContactPage = () => {
         Contact Us
       </h2>
       <p className="mb-3">If you enter your details we'll get back to you as soon as we can.</p>
-      <form onSubmit={submitHandler}>
+      <Form method="post">
         <div className={fieldStyle}>
           <label htmlFor="name">Your name</label>
-          <input
-            type="text"
-            id="name"
-            value={contact.name}
-            onChange={(e) => setContact({ ...contact, name: e.target.value })}
-          />
+          <input type="text" id="name" name="name" />
         </div>
         <div className={fieldStyle}>
           <label htmlFor="email">Your email address</label>
-          <input
-            type="text"
-            id="email"
-            value={contact.email}
-            onChange={(e) => setContact({ ...contact, email: e.target.value })}
-          />
+          <input type="text" id="email" name="email" />
         </div>
 
         <div className={fieldStyle}>
           <label htmlFor="reason">Reason you need to contact us</label>
-          <select
-            id="reason"
-            value={contact.reason}
-            onChange={(e) => setContact({ ...contact, reason: e.target.value })}
-          >
+          <select id="reason" name="reason">
             <option value=""></option>
             <option value="Support">Support</option>
             <option value="Feedback">Feedback</option>
@@ -155,11 +213,7 @@ const ContactPage = () => {
 
         <div className={fieldStyle}>
           <label htmlFor="notes">Additional notes</label>
-          <textarea
-            id="notes"
-            value={contact.notes}
-            onChange={(e) => setContact({ ...contact, notes: e.target.value })}
-          />
+          <textarea id="notes" name="notes" />
         </div>
 
         <div className={fieldStyle}>
@@ -171,9 +225,22 @@ text-white"
             Submit
           </button>
         </div>
-      </form>
+      </Form>
     </div>
   );
 };
 
 export default ContactPage;
+
+export async function contactPageAction({ request }: ActionFunctionArgs) {
+  const formData = await request.formData();
+  const contact = {
+    name: formData.get('name'),
+    email: formData.get('email'),
+    reason: formData.get('reason'),
+    notes: formData.get('notes'),
+  } as Contact;
+  console.log('Submitted details:', contact);
+
+  return redirect(`/thank-you/${formData.get('name')}`);
+}
