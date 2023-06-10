620ea6dff375859de7271541b55ec489f4db78bd
Sepide Kia <sepidekia6990@gmail.com>
2023-06-10 21:31:27 +0330

    add get posts list

--- myapp/src/index.tsx
+++ myapp/src/index.tsx
@@ -3,7 +3,8 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 // import { Routes } from './Routes';//Router-chapter
 // import App from './form/App'; //Form-chapter
-import App from './stateManagement/App'; //State management-chapter
+// import App from './stateManagement/App'; //State management-chapter
+import App from './rest/App'; //rest API chapter
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
---
+++ myapp/src/rest/posts/PostsPage.tsx
@@ -0,0 +1,31 @@
+import { useEffect, useState } from 'react';
+import { getPosts } from './getPosts';
+import { PostsList } from './PostsList';
+import { PostData } from '../types';
+
+export function PostsPage() {
+  const [isLoading, setIsLoading] = useState(true);
+  const [posts, setPosts] = useState<PostData[]>([]);
+  useEffect(() => {
+    let cancel = false;
+    getPosts().then((data) => {
+      if (!cancel) {
+        setPosts(data);
+        setIsLoading(false);
+      }
+    });
+    return () => {
+      cancel = true;
+    };
+  }, []);
+
+  if (isLoading) {
+    return <div className="w-96 mx-auto mt-6">Loading ...</div>;
+  }
+  return (
+    <div className="w-96 mx-auto mt-6">
+      <h2 className="text-xl text-slate-900 font-bold">Posts</h2>
+      <PostsList posts={posts} />
+    </div>
+  );
+}
